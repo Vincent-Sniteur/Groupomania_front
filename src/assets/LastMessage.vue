19e6462d8890bbd923523cd322eb830dca2532f2
BuildTools <vincent.sniteur@gmail.com>
2022-07-29 10:15:15 +0200

    Clean design for message wall & add button edit,delete, admin

--- src/assets/LastMessage.vue
+++ src/assets/LastMessage.vue
@@ -14,96 +14,73 @@ export default {
 
 <template>
 <div class="tab-pane active" id="home" role="tabpanel" aria-expanded="true">
+<!-- EXEMPLE MESSAGE NO ATTACHED FILE -->
+    <div class="media Tweets post-comment">
+        <!-- Profil img -->
+        <img class="mr-3 rounded-circle img-flex" src="https://picsum.photos/50/50" alt="User Avatar" width="50" height="50">
+
+        <!-- Message body -->
+        <h5 class="post-comment-username card-title">Utilisateur 1</h5>
+
+        <!-- Attached file -->
+        <!-- <div class="post-comment-attach">
+            <img class="card-img-top mb-1" src="https://picsum.photos/1080/920" alt="Attached file">
+        </div> -->
+
+        <!-- Timestamp & Rport -->
+        <p class="post-comment-timestamp">25 mins ago
+            <!-- Edit button -->
+            <router-link to="#" class="edit-button">Edit</router-link>
+            <!-- Delete button -->
+            <router-link to="#" class="delete-button">Delete</router-link>
+            <!-- Admin button -->
+            <router-link to="#" class="admin-button">Ban User</router-link>
+        </p>
+        
+
+        <!-- Message -->
+        <p class="post-comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        
+        <!-- Heart -->
+        <div class="placement">
+            <div class="heart is-active"></div>
+        </div>
+    </div>
+<!-- END EXEMPLE MESSAGE -->
+
 <!-- EXEMPLE MESSAGE -->
     <div class="media Tweets post-comment">
+        <!-- Profil img -->
+        <img class="mr-3 rounded-circle img-flex" src="https://picsum.photos/50/50" alt="User Avatar" width="50" height="50">
+
+        <!-- Message body -->
+        <h5 class="post-comment-username card-title">Utilisateur 1</h5>
 
         <!-- Attached file -->
         <div class="post-comment-attach">
             <img class="card-img-top mb-1" src="https://picsum.photos/1080/920" alt="Attached file">
         </div>
 
-        <!-- Profil img -->
-        <img class="mr-3 rounded-circle img-flex" src="https://picsum.photos/50/50" alt="User Avatar" width="50" height="50">
+        <!-- Timestamp & Rport -->
+        <p class="post-comment-timestamp">25 mins ago
+            <!-- Edit button -->
+            <router-link to="#" class="edit-button">Edit</router-link>
+            <!-- Delete button -->
+            <router-link to="#" class="delete-button">Delete</router-link>
+            <!-- Admin button -->
+            <router-link to="#" class="admin-button">Ban User</router-link>
+        </p>
+        
 
-        <!-- Message body -->
-        <div class="media-body">
-            <!-- Username & Timestamp / report -->
-            <h5 class="post-comment-username card-title">Utilisateur 1</h5>
-            <p class="post-comment-timestamp">25 mins ago 
-                <router-link to="#" class="report-button">Report</router-link>
-            </p>
-            
-            <!-- Message -->
-            <p class="post-comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-
+        <!-- Message -->
+        <p class="post-comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        
         <!-- Heart -->
         <div class="placement">
             <div class="heart is-active"></div>
         </div>
     </div>
 <!-- END EXEMPLE MESSAGE -->
-<!-- EXEMPLE MESSAGE -->
-    <div class="media Tweets post-comment">
-
-        <!-- Attached file -->
-        <div class="post-comment-attach">
-            <img class="card-img-top mb-1" src="https://picsum.photos/1080/920" alt="Attached file">
-        </div>
-
-        <!-- Profil img -->
-        <img class="mr-3 rounded-circle img-flex" src="https://picsum.photos/50/50" alt="User Avatar" width="50" height="50">
-
-        <!-- Message body -->
-        <div class="media-body">
-            <!-- Username & Timestamp / report -->
-            <h5 class="post-comment-username card-title">Utilisateur 1</h5>
-            <p class="post-comment-timestamp">25 mins ago 
-                <router-link to="#" class="report-button">Report</router-link>
-            </p>
-            
-            <!-- Message -->
-            <p class="post-comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-
-        <!-- Heart -->
-        <div class="placement">
-            <div class="heart is-active"></div>
-        </div>
-    </div>
-<!-- END EXEMPLE MESSAGE -->
-<!-- EXEMPLE MESSAGE NO ATTACHED FILE-->
-    <div class="media Tweets post-comment">
-
-        <!-- Attached file -->
-        <div class="post-comment-attach">
-            <!-- <img class="card-img-top mb-1" src="https://picsum.photos/1080/920" alt="Attached file"> -->
-        </div>
-
-        <!-- Profil img -->
-        <img class="mr-3 rounded-circle img-flex" src="https://picsum.photos/50/50" alt="User Avatar" width="50" height="50">
-
-        <!-- Message body -->
-        <div class="media-body">
-            <!-- Username & Timestamp / report -->
-            <h5 class="post-comment-username card-title">Utilisateur 1</h5>
-            <p class="post-comment-timestamp">25 mins ago 
-                <router-link to="#" class="report-button">Report</router-link>
-            </p>
-            
-            <!-- Message -->
-            <p class="post-comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-
-        <!-- Heart -->
-        <div class="placement">
-            <div class="heart is-active"></div>
-        </div>
-    </div>
-<!-- END EXEMPLE MESSAGE -->
-
-
-
 
 </div>
 </template>
@@ -112,10 +89,10 @@ export default {
 <style scoped>
 /* POST */ 
 .post-comment {
-    border-top: 1px solid #dbdbdb;
-    border-bottom: 1px solid #dbdbdb;
+    border-top: 2px solid #4E5166;
+    border-bottom: 2px solid #4E5166;
     position: relative;
-    padding: 5px 0 20px 0;
+    padding: 10px 0 20px 0;
 }
 .post-comment-username {
     color: #363636;
@@ -126,7 +103,7 @@ export default {
     margin-inline-start: 3px;
 }
 .post-comment-timestamp {
-    color: #8b8b8b;
+    color: #4E5166;
     font-size: .6875em;
     line-height: 1em;
     margin-inline-start: 3px;
@@ -140,19 +117,27 @@ export default {
     margin-inline-start: 5px;
     margin-inline-end: 0px;
 }
-.report-button {
+.edit-button {
     margin-left: 8px;
-    color: #ffa6a6;
+    color: #4E5166;
+}
+.delete-button {
+    margin-left: 8px;
+    color: #FD2D01;
+}
+.admin-button {
+    margin-left: 8px;
+    color: #FD2D01;
 }
 
 /* ATTACH FILE IMG */
 .post-comment-attach img {
     width: 100%;
-    height: 200px;
+    max-height: 250px;
     object-fit: cover;
+    object-position: center;
+    border-radius: 20px;
 }
-
-
 
 /* LIKE BUTTON */
 .heart {
